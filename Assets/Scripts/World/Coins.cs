9a20b3611ab2d68100709c905e3f1727613a5e77
Daniel Gallagher <g00360986@gmit.ie>
2021-04-21 16:20:54 +0100

    Menu added

---
+++ Assets/Scripts/World/Coins.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            //or gameObject.SetActive(false);
+        }
+    }
+}
