7199568f122b809b875e25c53e3dfc3e2b021dea
Daniel Gallagher <g00360986@gmit.ie>
2021-04-22 14:03:38 +0100

    Coin sounds added + Back movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public AudioClip coinSound;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("GoldCoin"))
         {
+            //Plays a sound for the coin
+            //AudioSource.PlayClipAtPoint(coinSound, transform.position);
+            //Destorys the game object after a collision (ie the coin)
             Destroy(other.gameObject);
         }
     }
--- Assets/Scripts/World/Coin.cs
+++ Assets/Scripts/World/Coin.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int coinValue = 1;
+    public AudioClip coinSound;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
+            //Plays a sound for the coin
+            AudioSource.PlayClipAtPoint(coinSound, transform.position);
+            //Put the score up
             ScoreManager.instance.ChangeScore(coinValue);
         }
     }
