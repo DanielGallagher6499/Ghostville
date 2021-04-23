5aba0094b4b3ae91ca28add5138a99765567f720
Daniel Gallagher <g00360986@gmit.ie>
2021-04-23 12:37:37 +0100

    Menu voice commands added + UI work

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+//Daniel Gallagher - G00360986
+//Ghostville
+
+public class MenuController : MonoBehaviour
+{
+    //Variables
+    public GrammarRecognizer gr;
+    private string _outAction;
+
+
+    public void Update()
+    {
+        //Update switch for forward and backward movement also including stop
+        switch (_outAction)
+        {
+            case "play":
+                Play();
+                break;
+            case "quit":
+                Quit();
+                break;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gr = new GrammarRecognizer(Path.Combine(Application.streamingAssetsPath, "menuGrammar.xml"), ConfidenceLevel.Low);
+        gr.OnPhraseRecognized += GR_OnPhraseRecognized;
+        gr.Start();
+        Debug.Log("Grammar loaded and recogniser started!");
+
+    }
+
+
+    private void GR_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        //Phase recognizer
+        StringBuilder message = new StringBuilder();
+        Debug.Log("Recognised a phrase");
+
+        // read the semantic meanings from the args passed in.
+        SemanticMeaning[] meanings = args.semanticMeanings;
+
+        // semantic meanings are returned as key/value pairs
+        // use foreach to get all the meanings.
+        foreach (SemanticMeaning meaning in meanings)
+        {
+            string keyString = meaning.key.Trim();
+            string valueString = meaning.values[0].Trim();
+            message.Append("Key: " + keyString + ", Value: " + valueString + " ");
+            _outAction = valueString;
+        }
+        // use a string builder to create the string and out put to the user
+        Debug.Log(message);
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (gr != null && gr.IsRunning)
+        {
+            gr.OnPhraseRecognized -= GR_OnPhraseRecognized;
+            gr.Stop();
+        }
+    }
+
+   private void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed!");
+    }
+}
