814304c0da8c48860eb1958a8791625103979982
Daniel Gallagher <g00360986@gmit.ie>
2021-04-23 16:26:21 +0100

    Game development complete

---
+++ Assets/Scripts/Controllers/WinMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
